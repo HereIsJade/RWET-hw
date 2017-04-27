e4711c49f0718d3a0fcd0da6e25b0815c09dbdd4
Jade <jingfeilin@me.com>
2017-04-27 04:54:57 -0400

    v2.0

--- final/app.py
+++ final/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request,render_template
 import quoteScraper as qs
-
+import textGenerator as tg
 
 app=Flask(__name__)
 
@@ -10,9 +10,23 @@ def home():
 
 @app.route('/create')
 def create():
-    name=request.args.get('name','')
-    poem=qs.scrape(name)
-    return render_template('home.html',poem=poem)
+    # if request.form['submit'] == 'Answer Me':
+    #     name=request.args.get('name','')
+    #     question,answer=tg.getQA(name)
+    # elif request.form['submit'] == 'One More Time':
+    #         pass # do something else
+    nameQ=request.args.get('nameQ','')
+    nameA=request.args.get('nameA','')
+    questions,a=tg.getQA(nameQ)
+    q,answers=tg.getQA(nameA)
+    QandA=''
+    for i in range(10):
+        question=questions[i]+"<br/>"
+        answer=answers[i]+"<br/>"
+
+        QandA=QandA+question+answer
+
+    return render_template('home.html',QandA=QandA)
 
 if __name__ =='__main__':
     app.run(debug=True)
---
+++ final/textGenerator.py
@@ -0,0 +1,50 @@
+from __future__ import unicode_literals
+import spacy
+import quoteScraper
+import random
+
+nlp = spacy.load('en')
+wordDict={'verbs':[],'nouns':[],'adjs':[],'advs':[],'prons':[]}
+
+def getName(name):
+    quoteFile = open(quoteScraper.scrape(name),"r")
+    texts = quoteFile.read()
+
+    doc = nlp(texts.decode('utf8'))
+    sentences_as_list = list(doc.sents)
+    # print len(sentences_as_list)
+
+    for item in doc:
+        if item.tag_ == 'NN':
+            wordDict['nouns'].append(item.text)
+        elif item.pos_ == 'VERB':
+            wordDict['verbs'].append(item.lemma_)
+        elif item.pos_ == 'ADJ':
+            wordDict['adjs'].append(item.text)
+        elif item.tag_ == 'RB':
+            wordDict['advs'].append(item.text)
+        elif item.pos_ == 'PRON':
+            wordDict['prons'].append(item.text)
+
+def getQA(name):
+    getName(name)
+    questions=[]
+    answers=[]
+    for i in range(10):
+        question_list=[random.choice(wordDict['adjs']),random.choice(wordDict['nouns']),random.choice(wordDict['verbs']),random.choice(wordDict['advs']),random.choice(wordDict['adjs']),random.choice(wordDict['prons']),random.choice(wordDict['advs'])]
+
+        question=" ".join(question_list)
+
+        answer_list=[random.choice(wordDict['adjs']),random.choice(wordDict['nouns']),random.choice(wordDict['verbs']),random.choice(wordDict['adjs']),random.choice(wordDict['nouns'])]
+
+
+        answer=" ".join(answer_list)
+        answer=answer[0].upper()+answer[1:]+'.'
+        question="What if "+question+"?"
+        questions.append(question)
+        answers.append(answer)
+    return questions, answers
+    # print answer
+
+# b = TextBlob(generated)
+# print b.correct()
